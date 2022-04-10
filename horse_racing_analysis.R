dba334f2a36b33680c85a42095cdcea88f05b11c
owen-j-looney <owen.j.looney@gmail.com>
2022-04-10 16:13:40 +1000

    extracted all data required

--- horse_racing_analysis.R
+++ horse_racing_analysis.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(purrr)
+library(curl)
+library(glue)
 
 #getting list of 
 base_url <- "https://www.racingaustralia.horse/FreeFields/Calendar_Results.aspx"
@@ -25,7 +27,7 @@ for (i in 1:length(state_list)) {
   state_list_url[[i]] <- paste0(base_url,"?State=", state_list[i])
 }
 
-
+#generating a list of urls to run through to get all race data for last 30 days
 results <- function(url_list){
   paste0("https://www.racingaustralia.horse",
          read_html(url_list) %>%
@@ -37,20 +39,62 @@ results <- function(url_list){
   )
 }
 
+#getting list of all urls with race data and converting to one list
 race_results_urls <- map(state_list_url, results) %>%
-  flatten()
+  flatten() 
 
-test1 <- read_html(race_results_urls[[1]]) %>%
-  html_table()
-  html_nodes("table") %>%
-  xml_find_all(xpath = '//*[@id="page-content/table"]')
-  #html_nodes(xpath = '//*[@id="page-content"]') %>%
-  #html_elements(".race-title") %>%
-  html_table()
-  html_elements(xpath = '/html/body/div[1]/div/div[4]/div/div/div/table[1]') %>%
-  html_table(xpath = "/html/body/div[1]/div/div[4]/div/div/div/table[1]")
-  html_children() %>%
-  html_attrs()
-  html_name()
-  html_nodes('table') 
-  html_table()
+#separating out trial and race data
+trial_results_urls <- race_results_urls[grepl("Trial", race_results_urls)]
+
+racing_results_urls <- race_results_urls[!grepl("Trial", race_results_urls)]
+
+#creating function to extract all tables from each url 
+read_html_tbls <- function(url) {
+  url = url(url, 'rb')
+  html <- read_html(url) %>%
+    html_table()
+  
+  close(url)
+  return(html)
+}
+
+#creating blank list for race tables to be appended to
+race_tables <- c()
+
+#appending all race tables
+race_tables <- map(racing_results_urls,read_html_tbls)
+
+#naming each group of tables with their date and location
+names(race_tables) <- sub('.*Key=','',racing_results_urls)
+
+#removing abandoned races with no data
+race_tables_non_abndn <- Filter(function(x) length(x) > 1,race_tables)
+
+
+#filtering out tables that have non race results or race information
+#appending race info table as a character string to the relevant race so it can be used
+for (n in 1:length(race_tables_non_abndn)){
+  if (ncol(race_tables_non_abndn[[n]][[1]])==4){
+    race_tables_non_abndn[[n]] <- race_tables_non_abndn[[n]][-1]
+  } else {
+    NULL
+  }
+  for (i in 1:length(race_tables_non_abndn[[n]])) {
+    if ((i %% 2)!=0){
+      as.character(race_tables_non_abndn[[n]][[i]])
+      } else {
+        race_tables_non_abndn[[n]][[i]] <- race_tables_non_abndn[[n]][[i]] %>%
+          mutate(character = as.character(race_tables_non_abndn[[n]][[i-1]]),
+                 info = glue("{names(race_tables_non_abndn)[[n]]},race{i}"))
+      }
+  }
+}
+
+tst_race_tbl1 <- race_tables_non_abndn[[1]][[2]]
+
+test1 <- Filter(function(x) ncol(x) > 1,race_tables_non_abndn)
+  
+
+test2 <- lapply(race_tables_non_abndn, function(x) Filter(ncol(x)>2))
+
+
